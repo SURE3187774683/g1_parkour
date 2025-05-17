6a4fca4aca3034223c370dea307e477711d7dfe8
SURE3187774683 <3187774683@qq.com>
2025-05-17 11:32:37 +0800

    init

--- legged_gym/legged_gym/scripts/play.sh
+++ legged_gym/legged_gym/scripts/play.sh
@@ -2,9 +2,9 @@
 # DEVICE=cuda:0
 
 python /home/pc/workspace/g1_parkour/legged_gym/legged_gym/scripts/play.py \
---task g1_field \
---load_run May16_22-26-12_v1 \
---checkpoint 15500 \
+--task g1 \
+--load_run May16_21-12-31_Perlin \
+--checkpoint 2500 \
 # --headless
 
 # --load_run Mar20_16-45-53_track_1.7 \
